47319b199042727972ff0223c25e10f779b12d8e
Guilherme Diego <guidiego.expgames@gmail.com>
2016-10-26 16:22:02 -0200

    Added middleware to blocks settings IPs

--- app/settings.js
+++ app/settings.js
@@ -1,4 +1,4 @@
-export const ALLOW_ORIGIN = '*'
+export const ALLOW_ORIGIN = ['iclinic.com.br']
 export const DEFAULT_BUCKET = 'my-buckey'
 export const ACL_DEFAULT = 'public-read'
 export const AWS_REGION = 'sa-east-1'
