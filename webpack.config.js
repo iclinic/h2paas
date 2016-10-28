359f3f0f770c4388ac007423cb3020fea9bbb6e0
Guilherme Diego <guidiego.expgames@gmail.com>
2016-10-28 08:21:46 -0200

    Doing: Implement Apex from AWS Lambda

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  entry: './src/index.js',
+  target: 'node',
+  output: {
+    path: './lib',
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
+  },
+  externals: {
+    'aws-sdk': 'aws-sdk'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: [/node_modules/]
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      }
+    ]
+  }
+}
