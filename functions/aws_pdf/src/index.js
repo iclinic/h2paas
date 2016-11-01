9ed230d5f3b7fc90304e33c05dfcf76514491505
Guilherme Diego <guidiego.expgames@gmail.com>
2016-11-01 14:15:16 -0200

    Fix lambda failed calls

--- functions/aws_pdf/src/index.js
+++ functions/aws_pdf/src/index.js
@@ -1,5 +1,5 @@
-import { s3EmitPDF } from 'functions/s3'
-import { createPDF } from 'functions/core'
+import { s3EmitPDF } from '../../s3'
+import { createPDF } from '../../core'
 
 export default function(e, ctx) {
   return createPDF(e.template, e.options, s3EmitPDF(ctx))
